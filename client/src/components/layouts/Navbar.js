8a8fd4bf62e5ad93f509c7f64f643a650644a102
yash sharma <sharmayash44ys@gmail.com>
2019-01-20 21:37:11 +0530

    auth completed needs validation

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logOutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+  onLogOutClick = e => {
+    e.preventDefault();
+    this.props.logOutUser();
+  };
+
   render() {
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLink = (
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <Link to="/profile">Welcome {user.name}</Link>
+        </li>
+        <li>
+          <a href="!#" onClick={this.onLogOutClick}>
+            Log Out
+          </a>
+        </li>
+      </ul>
+    );
+
+    const sideAuthLink = (
+      <li>
+        <a href="!#" onClick={this.onLogOutClick}>
+          Log Out
+        </a>
+      </li>
+    );
+
+    const guestLink = (
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+        <li>
+          <Link to="/register">Sign Up</Link>
+        </li>
+      </ul>
+    );
+
     return (
       <div>
         <nav className="blue-grey darken-2">
@@ -13,15 +55,11 @@ class Navbar extends Component {
             <Link to="!#" data-target="slide-out" className="sidenav-trigger">
               <i className="material-icons">menu</i>
             </Link>
-            <ul className="right hide-on-med-and-down">
-              <li>
-                <Link to="/login">Log In</Link>
-              </li>
-              <li>
-                <Link to="/register">Sign Up</Link>
-              </li>
-            </ul>
-            <ul className="left hide-on-med-and-down" style={{marginLeft: '200px'}}>
+            {isAuthenticated ? authLink : guestLink}
+            <ul
+              className="left hide-on-med-and-down"
+              style={{ marginLeft: "150px", marginTop: "6px" }}
+            >
               <li>
                 <Link to="/phones">SmartPhones</Link>
               </li>
@@ -43,13 +81,10 @@ class Navbar extends Component {
             <div className="user-view">
               <div className="background img1" />
               <Link to="/profile">
-                <img className="circle img2" alt="dp"/>
+                <img className="circle img2" alt="" />
               </Link>
               <Link to="/profile">
-                <span className="white-text name">John Doe</span>
-              </Link>
-              <Link to="/profile">
-                <span className="white-text email">jdandturk@gmail.com</span>
+                <span className="white-text name">Welcome {user.name}</span>
               </Link>
             </div>
           </li>
@@ -81,12 +116,18 @@ class Navbar extends Component {
           <li>
             <div className="divider" />
           </li>
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
-          <li>
-            <Link to="/register">Sign Up</Link>
-          </li>
+          {isAuthenticated ? (
+            sideAuthLink
+          ) : (
+            <div>
+              <li>
+                <Link to="/login">Log In</Link>
+              </li>
+              <li>
+                <Link to="/register">Sign Up</Link>
+              </li>
+            </div>
+          )}
           <li>
             <div className="divider" />
           </li>
@@ -96,4 +137,16 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logOutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logOutUser }
+)(Navbar);
--- client/src/reducers/authReducers.js
+++ client/src/reducers/authReducers.js
@@ -1,3 +1,5 @@
+import { SET_CURRENT_USER } from "../actions/types";
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,6 +7,12 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !state.isAuthenticated,
+        user: action.payload
+      };
     default:
       return state;
   }
