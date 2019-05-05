1274e9fa9b054d1f579dc110f01a5f0508969a14
yash sharma <sharmayash44ys@gmail.com>
2019-05-05 20:31:48 +0530

    change

--- client/src/components/products/ProductItem.js
+++ client/src/components/products/ProductItem.js
@@ -4,15 +4,18 @@ import { Link } from "react-router-dom";
 
 class ProductItem extends Component {
   render() {
-    const { products } = this.props;
+    const data = Array.from(this.props.products);
 
-    return products.map(product => (
+    return data.map(product => (
       <div className="col s12 m6 l4" key={product._id}>
         <div className="card cardpadding">
           <div className="card-image center">
             <img src={product.image} alt={product.name} className="cardImage" />
             <span className="card-title">{product.name}</span>
-            <Link to="#" className="btn-floating halfway-fab waves-effect waves-light btn-large deep-purple lighten-5 black-text z-index-1">
+            <Link
+              to="#"
+              className="btn-floating halfway-fab waves-effect waves-light btn-large deep-purple lighten-5 black-text z-index-1"
+            >
               $ {product.price}
             </Link>
           </div>
