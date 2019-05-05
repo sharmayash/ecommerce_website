57a53c2345c7246374a4e0cb08537c72d74fd704
yash sharma <sharmayash44ys@gmail.com>
2019-05-05 23:41:49 +0530

    make it better

--- client/src/components/products/ProductItem.js
+++ client/src/components/products/ProductItem.js
@@ -5,26 +5,31 @@ import { Link } from "react-router-dom";
 class ProductItem extends Component {
   render() {
     // const data = Array.from(this.props.products);
-
-    return this.props.products.map(product => (
-      <div className="col s12 m6 l4" key={product._id}>
-        <div className="card cardpadding">
-          <div className="card-image center">
-            <img src={product.image} alt={product.name} className="cardImage" />
-            <span className="card-title">{product.name}</span>
-            <Link
-              to="#"
-              className="btn-floating halfway-fab waves-effect waves-light btn-large deep-purple lighten-5 black-text z-index-1"
-            >
-              $ {product.price}
-            </Link>
-          </div>
-          <div className="card-action">
-            <Link to={`/product/${product._id}`}>View</Link>
+    if (this.props.products) {
+      return this.props.products.map(product => (
+        <div className="col s12 m6 l4" key={product._id}>
+          <div className="card cardpadding">
+            <div className="card-image center">
+              <img
+                src={product.image}
+                alt={product.name}
+                className="cardImage"
+              />
+              <span className="card-title">{product.name}</span>
+              <Link
+                to="#"
+                className="btn-floating halfway-fab waves-effect waves-light btn-large deep-purple lighten-5 black-text z-index-1"
+              >
+                $ {product.price}
+              </Link>
+            </div>
+            <div className="card-action">
+              <Link to={`/product/${product._id}`}>View</Link>
+            </div>
           </div>
         </div>
-      </div>
-    ));
+      ));
+    }
   }
 }
 
